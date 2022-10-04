442fe0a2216965c4d0948ef9d72d3eb8765e0547
Håkon Nese <haakon.nese@gmail.com>
2022-10-04 09:52:15 +0200

    Fix lint

--- src/components/elements/SensorData/SensorGraph.tsx
+++ src/components/elements/SensorData/SensorGraph.tsx
@@ -1,25 +1,25 @@
-import "chartjs-adapter-moment"
+import 'chartjs-adapter-moment'
 import {
-    Chart as ChartJS,
-    TimeScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-  } from "chart.js"
+  Chart as ChartJS,
+  TimeScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
 import { Line } from 'react-chartjs-2'
 
 ChartJS.register(
-    TimeScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-  );
+  TimeScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
 
 //   const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
@@ -42,66 +42,62 @@ ChartJS.register(
 //   ],
 // };
 
-  export const options = {
-    responsive: true,
-    elements: {
-        point:{
-            radius: 0
-        }
-    },
-    scales: {
-        x: {
+export const options = {
+  responsive: true,
+  elements: {
+    point: {
+      radius: 0
+    }
+  },
+  scales: {
+    x: {
 
-            type: 'time',
-            time: {
-                unit: 'day',
-                tooltipFormat: 'YYYY-MM-DD HH:mm',
+      type: 'time',
+      time: {
+        unit: 'day',
+        tooltipFormat: 'YYYY-MM-DD HH:mm'
 
-            },
-            grid: {
-              color: 'white'
-            }
-
-        },
-        y: {
-          grid: {
-            color: 'white'
-          }
-        }
-
-
-    },
-    plugins: {
-      legend: {
-        position: 'top' as const,
       },
-      title: {
-        display: true,
-        text: 'Temprature at parkinglot',
-      },
+      grid: {
+        color: 'white'
+      }
+
     },
-  };
+    y: {
+      grid: {
+        color: 'white'
+      }
+    }
 
-
-
-  interface SensorParams {
-    time: string[]
-    measurments: number[]
+  },
+  plugins: {
+    legend: {
+      position: 'top' as const
+    },
+    title: {
+      display: true,
+      text: 'Temprature at parkinglot'
+    }
   }
-  export function SensorGraph({time, measurments}: SensorParams) {
+}
 
-    console.log(measurments)
-    const data = {
-        labels: time.map(t=>(new Date(t+"Z"))),
-        datasets: [
-          {
-            label: 'Temperature',
-            data: measurments,
-            borderColor: 'blue',
-            backgroundColor: 'white',
-          },
-        ],
-      };
-    // @ts-ignore
-    return <Line options={options} data={data} />;
+interface SensorParams {
+  time: string[]
+  measurments: number[]
+}
+export function SensorGraph ({ time, measurments }: SensorParams): JSX.Element {
+  console.log(measurments)
+  const data = {
+    labels: time.map(t => (new Date(t + 'Z'))),
+    datasets: [
+      {
+        label: 'Temperature',
+        data: measurments,
+        borderColor: 'blue',
+        backgroundColor: 'white'
+      }
+    ]
   }
+  // @ts-expect-error
+  return <Line options={options} data={data} />
+}
