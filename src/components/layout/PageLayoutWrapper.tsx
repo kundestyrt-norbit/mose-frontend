fc25cf9cfdedb79bce28b01704109f82038fb174
Kasper Lien Oftebro <kaspero@Kaspers-MacBook-Pro.local>
2022-10-04 10:15:13 +0200

    Integrated openstreetmap and som dashboard-styling

--- src/components/layout/PageLayoutWrapper.tsx
+++ src/components/layout/PageLayoutWrapper.tsx
@@ -1,6 +1,5 @@
 import { styled } from '@mui/system'
 import React, { useEffect } from 'react'
-import { Box } from '@mui/material'
 import Navbar from './NavBar/NavBar'
 interface PageLayoutWrapperProps {
   children: React.ReactNode
@@ -12,19 +11,6 @@ interface PageLayoutWrapperProps {
 const PageLayoutWrapper = ({
   children
 }: PageLayoutWrapperProps): JSX.Element => {
-  // const [sidebarIsOpen, setSidebarIsOpen] = useState<boolean>(true)
-
-  // const toggleDrawer =
-  //   (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent | undefined) => {
-  //     if (event !== undefined &&
-  //       event.type === 'keydown' &&
-  //       ((event as React.KeyboardEvent).key === 'Tab' ||
-  //         (event as React.KeyboardEvent).key === 'Shift')) {
-  //       return
-  //     }
-  //     setSidebarIsOpen(open)
-  //   }
-  // Use effect for setting height values on resize
   useEffect(() => {
     const handleResize = (): void => {
       document.body.setAttribute('style', `--100vh: ${window.innerHeight}px;`)
@@ -38,42 +24,17 @@ const PageLayoutWrapper = ({
   }, [])
   return (
     <FullPageWrapper>
-      {/* <Navbar onSidebarOpen={() => setSidebarIsOpen(true)} /> */}
       <Navbar />
-      {/* <Sidebar toggleDrawer={toggleDrawer} open={sidebarIsOpen} /> */}
-      <PageLayoutRoot>
-        <Box
-          sx={{
-            display: 'flex',
-            flex: '1 1 auto',
-            flexDirection: 'column',
-            width: '100%'
-          }}
-        >
-          {children}
-        </Box>
-      </PageLayoutRoot>
+      {children}
     </FullPageWrapper>
   )
 }
 
-const PageLayoutRoot = styled('div')(({ theme }) => ({
-  display: 'flex',
-  flex: '1 1 auto',
-  maxWidth: '100%',
-  paddingTop: 64,
-  [theme.breakpoints.up('lg')]: {
-    paddingLeft: 280
-  }
-}))
-
 const FullPageWrapper = styled('div')`
-  height: var(--100vh);
   width: 100%;
+  height: 100%;
   display: flex;
-
-  overflow: auto;
-  position: relative;
-`
+  flex-flow: column;
+  `
 
 export default PageLayoutWrapper
