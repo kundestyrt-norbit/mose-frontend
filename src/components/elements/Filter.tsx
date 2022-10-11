980eeb669fee5f6529a771edee91412a67ae86eb
Kasper Lien Oftebro <kasper.oftebro@gmail.com>
2022-10-11 12:55:24 +0200

    lint issues fixed

--- src/components/elements/Filter.tsx
+++ src/components/elements/Filter.tsx
@@ -8,35 +8,35 @@ import CheckBoxIcon from '@mui/icons-material/CheckBox'
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />
 const checkedIcon = <CheckBoxIcon fontSize="small" />
 
-export default function CheckboxesTags() {
-    return (
-        <Autocomplete
-            multiple
-            id="checkboxes-tags-demo"
-            options={dashboards}
-            disableCloseOnSelect
-            getOptionLabel={(option) => option.title}
-            renderOption={(props, option, { selected }) => (
-                <li {...props}>
-                    <Checkbox
-                        icon={icon}
-                        checkedIcon={checkedIcon}
-                        style={{ marginRight: 8, color: 'white' }}
-                        checked={selected}
-                    />
-                    {option.title}
-                </li>
-            )}
-            style={{ minWidth: '30%', margin: '3%' }}
-            renderInput={(params) => (
-                <TextField {...params} label="Add to dashboard" placeholder="Dashboards" />
-            )}
-        />
-    )
+export default function CheckboxesTags (): JSX.Element {
+  return (
+    <Autocomplete
+      multiple
+      id="checkboxes-tags-demo"
+      options={dashboards}
+      disableCloseOnSelect
+      getOptionLabel={(option) => option.title}
+      renderOption={(props, option, { selected }) => (
+        <li {...props}>
+          <Checkbox
+            icon={icon}
+            checkedIcon={checkedIcon}
+            style={{ marginRight: 8, color: 'white' }}
+            checked={selected}
+          />
+          {option.title}
+        </li>
+      )}
+      style={{ minWidth: '30%', margin: '3%' }}
+      renderInput={(params) => (
+        <TextField {...params} label="Add to dashboard" placeholder="Dashboards" />
+      )}
+    />
+  )
 }
 
 const dashboards = [
-    { title: 'Dashboard 1' },
-    { title: 'Dashboard 2' },
-    { title: 'Dashboard 3' }
+  { title: 'Dashboard 1' },
+  { title: 'Dashboard 2' },
+  { title: 'Dashboard 3' }
 ]
