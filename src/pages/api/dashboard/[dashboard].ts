5fbb48b712164a940177e24927d0adf4d16d599a
larsmathisen <6087277+larsmathisen@users.noreply.github.com>
2022-10-18 11:40:44 +0200

    Remove logging of session

--- src/pages/api/dashboard/[dashboard].ts
+++ src/pages/api/dashboard/[dashboard].ts
@@ -5,7 +5,6 @@ import { getDashboard, saveDashboard } from './_queryUserSettings'
 
 export default async function handler (req: NextApiRequest, res: NextApiResponse): Promise<void> {
   const { Auth } = withSSRContext({ req })
-  console.log(await Auth.currentSession())
   const userId: string | null = await getVerifiedUserID(Auth)
   if (userId != null) {
     if (req.method === 'PUT') {
