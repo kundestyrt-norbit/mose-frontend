98d2cc164f8f1af3dce472ff2b1d2a8931fe7e1a
TrymT <tiltrym@gmail.com>
2022-10-27 13:45:59 +0200

    chore: improve sensor list view

--- src/pages/api/sensor/_sensorMetaData.ts
+++ src/pages/api/sensor/_sensorMetaData.ts
@@ -18,7 +18,7 @@ export const SensorMetaDataMap: {[key: string]: SensorMetaData} = {
   mc_2_5: {
     friendlyName: 'PM 2.5',
     unit: 'μg/m&sup3',
-    description: 'Mass concentration of particles with size 0.3 to 2.5 μm'
+    description: 'Mass concentration of particles with size 0.3 to 2.5 μm.'
   },
   mc_10_0: {
     friendlyName: 'PM 10',
@@ -29,43 +29,36 @@ export const SensorMetaDataMap: {[key: string]: SensorMetaData} = {
     friendlyName: 'PM 4',
     unit: 'μg/m&sup3',
     description: 'Mass concentration of particles with size 0.3 to 4 μm'
-
   },
   nc_10_0: {
     friendlyName: 'PM 10',
     unit: '#/m&sup3',
-    description: 'Number concentration of particles with size 0.3 to 10 μm'
-
+    description: 'Number concentration of particles with size 0.3 to 10 μm.'
   },
   mc_1_0: {
     friendlyName: 'PM 1',
     unit: 'μg/m&sup3',
-    description: 'Mass concentration of particles with size 0.3 to 1μm'
-
+    description: 'Mass concentration of particles with size 0.3 to 1μm.'
   },
   nc_0_5: {
     friendlyName: 'PM 0.5',
     unit: '#/m&sup3',
-    description: 'Number concentration of particles with size 0.3 to 0.5 μm'
-
+    description: 'Number concentration of particles with size 0.3 to 0.5 μm.'
   },
   nc_2_5: {
     friendlyName: 'PM 2.5',
     unit: '#/m&sup3',
-    description: 'Number concentration of particles with size 0.3 to 2.5 μm'
-
+    description: 'Number concentration of particles with size 0.3 to 2.5 μm.'
   },
   nc_1_0: {
     friendlyName: 'PM 1',
     unit: '#/m&sup3',
-    description: 'Number concentration of particles with size 0.3 to 1 μm'
-
+    description: 'Number concentration of particles with size 0.3 to 1 μm.'
   },
   nc_4_0: {
     friendlyName: 'PM 4',
     unit: '#/m&sup3',
-    description: 'Number concentration of particles with size 0.3 to 4 μm'
-
+    description: 'Number concentration of particles with size 0.3 to 4 μm.'
   },
   typical_particle_size: {
     friendlyName: 'Typical particle size',
