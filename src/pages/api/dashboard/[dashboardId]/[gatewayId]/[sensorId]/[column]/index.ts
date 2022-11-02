ce15b20a0db35b0124a1f09f30f71601f7c6ad5d
Håkon Nese <haakon.nese@gmail.com>
2022-11-02 17:20:32 +0100

    Endpoints not closed

--- src/pages/api/dashboard/[dashboardId]/[gatewayId]/[sensorId]/[column]/index.ts
+++ src/pages/api/dashboard/[dashboardId]/[gatewayId]/[sensorId]/[column]/index.ts
@@ -3,16 +3,17 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { withSSRContext } from 'aws-amplify'
 import { addSensor, deleteSensor } from '../../../_query'
 
-export default async function handler (req: NextApiRequest, res: NextApiResponse): Promise<void> {
+export default async function handler (req: NextApiRequest, res: NextApiResponse): Promise<NextApiResponse<any>> {
   const { Auth } = withSSRContext({ req })
   const userId: string | null = await getVerifiedUserID(Auth)
   if (userId != null) {
     if (req.method === 'PUT') {
       const item = await addSensor(req, userId)
-      return res.status(201).json(item)
+      return res.status(201).end(JSON.stringify(item))
     } else if (req.method === 'DELETE') {
       const deletedSensor: boolean = await deleteSensor(req, userId)
-      if (deletedSensor) { return res.status(204).json({}) } else return res.status(404).json({})
+      if (deletedSensor) { return res.status(204).end() } else return res.status(404).end()
     }
   }
+  return res.status(401).end()
 }
--- src/pages/api/dashboard/index.ts
+++ src/pages/api/dashboard/index.ts
@@ -3,16 +3,20 @@ import { withSSRContext } from 'aws-amplify'
 import { createDashboard } from './_queryUserSettings'
 import getVerifiedUserID from './_verifyUser'
 
-export default async function handler (req: NextApiRequest, res: NextApiResponse): Promise<void> {
+export default async function handler (req: NextApiRequest, res: NextApiResponse): Promise<NextApiResponse<any>> {
   const { Auth } = withSSRContext({ req })
   const userId: string | null = await getVerifiedUserID(Auth)
   if (userId != null) {
     if (req.method === 'PUT') {
-      const item = await createDashboard(req, userId)
-
-      return res.status(201).json(item)
+      return await createDashboard(req, userId).then(item => res.end(JSON.stringify(item))).catch(error => {
+        res.status(500)
+        return res.end(JSON.stringify(error))
+      })
     }
+    res.status(404)
+    return res.end()
   } else {
-    throw new Error('UserId not valid')
+    res.status(401)
+    return res.end()
   }
 }
