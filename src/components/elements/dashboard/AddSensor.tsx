563ad3603411ab04247ade16be0b4992494f269e
Kasper Lien Oftebro <kasper.oftebro@gmail.com>
2022-11-02 14:22:02 +0100

    Dashboard-view altered

---
+++ src/components/elements/dashboard/AddSensor.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react'
+import Checkbox from '@mui/material/Checkbox'
+import TextField from '@mui/material/TextField'
+import Autocomplete from '@mui/material/Autocomplete'
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank'
+import CheckBoxIcon from '@mui/icons-material/CheckBox'
+import { getSensorsIncludeDashboard } from '../../../utils/dashboardUtils'
+import { SensorIncludeDashboard } from '../../../pages/api/sensor/_queryClient'
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />
+const checkedIcon = <CheckBoxIcon fontSize="small" />
+
+export default function CheckboxesTags({ dashboardId, updateDashboard }: { dashboardId: string, updateDashboard: () => void }): JSX.Element {
+  const [sensorList, setSensorList] = React.useState<SensorIncludeDashboard[]>([])
+  const [value, setValue] = React.useState<SensorIncludeDashboard[]>([])
+  React.useEffect(() => {
+    getSensorsIncludeDashboard(dashboardId)
+      .then(res => setSensorList(res)).catch(err => console.log(err))
+  }, [])
+  React.useEffect(() => {
+    setValue(sensorList.filter(s => s.sensorIncludedInDashboard))
+  }, [sensorList])
+
+  return (
+    <Autocomplete
+      multiple
+      id="checkboxes-tags-demo"
+      options={sensorList}
+      disableCloseOnSelect
+      value={value}
+      onChange={(event, newValue, reason, detail) => {
+        setValue(newValue)
+        if (detail != null) {
+          if (reason === 'removeOption') {
+            fetch(`/api/dashboard/${dashboardId}/${detail.option.gatewayId}/${detail.option.id}/${detail.option.column}`,
+              {
+                method: 'DELETE',
+                headers: {
+                  'Content-type': 'application/json'
+                }
+              })
+              .then(() => updateDashboard()).catch(err => console.log(err))
+          } else if (reason === 'selectOption') {
+            fetch(`/api/dashboard/${dashboardId}/${detail.option.gatewayId}/${detail.option.id}/${detail.option.column}`,
+              {
+                method: 'PUT',
+                headers: {
+                  'Content-type': 'application/json'
+                }
+              })
+              .then(async res => await res.json()).then(() => updateDashboard()).catch(err => console.log(err))
+          }
+        }
+      }}
+      getOptionLabel={(option) => option.metaData?.friendlyName ?? ''}
+      renderOption={(props, option, { selected }) => (
+        <li {...props}>
+          <Checkbox
+            icon={icon}
+            checkedIcon={checkedIcon}
+            style={{ marginRight: 8, color: 'white' }}
+            checked={selected}
+          />
+          {option.metaData?.friendlyName}
+        </li>
+      )}
+      style={{ maxWidth: '100%', margin: '1% 3%' }}
+      renderInput={(params) => (
+        <TextField {...params} label="Add sensors" placeholder="Sensors" />
+      )}
+    />
+  )
+}
--- src/components/elements/dashboard/DashBoardView.tsx
+++ src/components/elements/dashboard/DashBoardView.tsx
@@ -5,16 +5,23 @@ const DashBoardView = ({ dashboard }: any): JSX.Element => {
   return (
     <div style={{
       display: 'flex',
-      flexDirection: 'column',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      minWidth: '45%',
       maxWidth: '100%',
-      justifyContent: 'center',
       alignItems: 'center',
-      margin: '1% 3%',
-      padding: '0'
+      margin: '1% 1%',
+      padding: '0',
+      justifyContent: 'space-evenly'
     }}>
-      <h1>{dashboard.dashboardName}</h1>
+      <h1 style={{ minWidth: '100%', textAlign: 'center' }}>{dashboard.dashboardName}</h1>
       {dashboard.sensors?.map((sensor: Sensor) => {
-        return <SensorGraph key={sensor.id.toString() + sensor.column} id={sensor.id} column={sensor.column} />
+        return (
+          <div key={sensor.id.toString() + sensor.column} style={{ flexDirection: 'column', textAlign: 'center', minWidth: '45%', border: '1px solid rgba(0, 0, 0, 0.229)', borderRadius: '4px', margin: '1% 0' }}>
+            <h2>{sensor.metaData?.friendlyName ?? sensor.column}</h2>
+            <SensorGraph id={sensor.id} column={sensor.column} />
+          </div>
+        )
       })}
     </div>
 
