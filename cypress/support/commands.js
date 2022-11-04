48e2bca44f081bf4465483ebe699bbdbe1b77fce
larsmathisen <6087277+larsmathisen@users.noreply.github.com>
2022-11-04 13:33:23 +0100

    Fix login support for testing

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,29 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
 const Auth = require ( "aws-amplify" ).Auth;
 import "cypress-localstorage-commands"; 
 const username = Cypress. env("username"); 
@@ -39,11 +13,11 @@ Auth. configure (awsconfig) ;
 Cypress.Commands.add("signIn", () => {
     cy.session('testLogin', () => {
         cy.then(() => Auth.signIn(username, password)).then((cognitoUser) => {
-            let { username, LastAuthUser } = cognitoUser
-            cy.setCookie(`CognitoIdentityServiceProvider.${cognitoUser.pool.clientId}.${cognitoUser.LastAuthUser}.${name}`, cognitoUser.LastAuthUser)
-            cy.setCookie(`CognitoIdentityServiceProvider.${cognitoUser.pool.clientId}.${username.accessToken}.${name}`, username.accessToken)
-            cy.setCookie(`CognitoIdentityServiceProvider.${cognitoUser.pool.clientId}.${username.idToken}.${name}`, username.idToken)
-            cy.setCookie(`CognitoIdentityServiceProvider.${cognitoUser.pool.clientId}.${username.refreshToken}.${name}`, username.refreshToken)
+          let { accessToken, idToken, refreshToken } = cognitoUser.signInUserSession
+          cy.setCookie(`CognitoIdentityServiceProvider.${cognitoUser.pool.clientId}.LastAuthUser`, cognitoUser.username)
+          cy.setCookie(`CognitoIdentityServiceProvider.${cognitoUser.pool.clientId}.${cognitoUser.username}.accessToken`, accessToken.jwtToken)
+          cy.setCookie(`CognitoIdentityServiceProvider.${cognitoUser.pool.clientId}.${cognitoUser.username}.idToken`, idToken.jwtToken)
+          // cy.setCookie(`CognitoIdentityServiceProvider.${cognitoUser.pool.clientId}.${cognitoUser.username}.refreshToken`, refreshToken)
         });
     })
   });
