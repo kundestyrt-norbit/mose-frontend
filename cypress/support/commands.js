b965d35fcc663633acc23d7b4abf9c9b6ca4850e
larsmathisen <6087277+larsmathisen@users.noreply.github.com>
2022-11-04 10:15:26 +0100

    Setup Cypres for testing

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,27 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+const Auth = require ( "aws-amplify" ).Auth;
+import "cypress-localstorage-commands"; 
+const username = Cypress. env("username"); 
+const password = Cypress. env("password"); 
+const userPoolId = Cypress. env("userPoolId"); 
+const clientId = Cypress. env ("clientId"); 
+const awsconfig = { 
+  aws_user_pools_id: userPoolId, 
+  aws_user_pools_web_client_id: clientId, 
+}; 
+Auth. configure (awsconfig) ;
+
+Cypress.Commands.add("signIn", () => {
+    cy.session('testLogin', () => {
+        cy.then(() => Auth.signIn(username, password)).then((cognitoUser) => {
+            let { username, LastAuthUser } = cognitoUser
+            cy.setCookie(`CognitoIdentityServiceProvider.${cognitoUser.pool.clientId}.${cognitoUser.LastAuthUser}.${name}`, cognitoUser.LastAuthUser)
+            cy.setCookie(`CognitoIdentityServiceProvider.${cognitoUser.pool.clientId}.${username.accessToken}.${name}`, username.accessToken)
+            cy.setCookie(`CognitoIdentityServiceProvider.${cognitoUser.pool.clientId}.${username.idToken}.${name}`, username.idToken)
+            cy.setCookie(`CognitoIdentityServiceProvider.${cognitoUser.pool.clientId}.${username.refreshToken}.${name}`, username.refreshToken)
+        });
+    })
+  });
