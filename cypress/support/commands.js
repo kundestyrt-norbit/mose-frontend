98b9bd4b2a9330d058706280093d65255c0352fe
larsmathisen <6087277+larsmathisen@users.noreply.github.com>
2022-11-05 14:05:14 +0100

    Change commands env variables for credentials

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,9 @@
 const Auth = require ( "aws-amplify" ).Auth;
 import "cypress-localstorage-commands"; 
-const username = Cypress. env("username"); 
-const password = Cypress. env("password"); 
-const userPoolId = Cypress. env("userPoolId"); 
-const clientId = Cypress. env ("clientId"); 
+const username = Cypress. env("CYPRESS_TEST_USERNAME"); 
+const password = Cypress. env("CYPRESS_TEST_PASSWORD"); 
+const userPoolId = Cypress. env("COGNITO_USER_POOL_ID"); 
+const clientId = Cypress. env ("COGNITO_CLIENT_APP_ID"); 
 const awsconfig = { 
   aws_user_pools_id: userPoolId, 
   aws_user_pools_web_client_id: clientId, 
