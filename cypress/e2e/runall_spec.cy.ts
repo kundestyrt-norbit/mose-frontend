02ed94ad98d77cbec6d08a48dab1eff0f154be9a
larsmathisen <6087277+larsmathisen@users.noreply.github.com>
2022-11-05 12:10:11 +0100

    Renaming and refactor

---
+++ cypress/e2e/runall_spec.cy.ts
@@ -0,0 +1,6 @@
+Cypress.session.clearAllSavedSessions()
+import './specs/login_spec.cy'
+import './specs/navigation_spec.cy'
+import './specs/dashboard_spec.cy'
+import './specs/sensordash_spec.cy'
+import './specs/alarm_spec.cy'
