460fc52e6babcbfd533f95359487ba5a7d3017d3
TrymT <tiltrym@gmail.com>
2022-11-05 14:40:11 +0100

    chore: change api request with hardcoded sensors

---
+++ src/pages/api/sensor/_existingSensors.ts
@@ -0,0 +1,77 @@
+import { Sensor } from '../../../components/elements/dashboard/types'
+import { SensorMetaDataMap } from './_sensorMetaData'
+
+export const ExistingSensors: Sensor[] = [
+  {
+    column: 'temperature',
+    id: 43,
+    metaData: SensorMetaDataMap.temperature,
+    gatewayId: 8
+  },
+  {
+    column: 'humidity',
+    id: 43,
+    metaData: SensorMetaDataMap.humidity,
+    gatewayId: 8
+  },
+  {
+    column: 'mc_2_5',
+    id: 145,
+    metaData: SensorMetaDataMap.mc_2_5,
+    gatewayId: 8
+  },
+  {
+    column: 'mc_10_0',
+    id: 145,
+    metaData: SensorMetaDataMap.mc_10_0,
+    gatewayId: 8
+  },
+  {
+    column: 'mc_4_0',
+    id: 145,
+    metaData: SensorMetaDataMap.mc_4_0,
+    gatewayId: 8
+  },
+  {
+    column: 'nc_10_0',
+    id: 145,
+    metaData: SensorMetaDataMap.nc_10_0,
+    gatewayId: 8
+  },
+  {
+    column: 'mc_1_0',
+    id: 145,
+    metaData: SensorMetaDataMap.mc_1_0,
+    gatewayId: 8
+  },
+  {
+    column: 'nc_0_5',
+    id: 145,
+    metaData: SensorMetaDataMap.nc_0_5,
+    gatewayId: 8
+  },
+  {
+    column: 'nc_2_5',
+    id: 145,
+    metaData: SensorMetaDataMap.nc_2_5,
+    gatewayId: 8
+  },
+  {
+    column: 'nc_1_0',
+    id: 145,
+    metaData: SensorMetaDataMap.nc_1_0,
+    gatewayId: 8
+  },
+  {
+    column: 'nc_4_0',
+    id: 145,
+    metaData: SensorMetaDataMap.nc_4_0,
+    gatewayId: 8
+  },
+  {
+    column: 'typical_particle_size',
+    id: 145,
+    metaData: SensorMetaDataMap.typical_particle_size,
+    gatewayId: 8
+  }
+]
